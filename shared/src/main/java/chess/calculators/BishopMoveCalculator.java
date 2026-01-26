26a2b77d9f041419f5ef0df3dc6c98257e8dfbe6
Denise Braithwaite <denisebraithw8@icloud.com>
2026-01-26 15:31:44 -0700

    refactor BishopMoveCalculator as subclass of PieceMovesCalculator

--- shared/src/main/java/chess/calculators/BishopMoveCalculator.java
+++ shared/src/main/java/chess/calculators/BishopMoveCalculator.java
@@ -7,35 +7,18 @@ import chess.ChessPosition;
 import java.util.ArrayList;
 import java.util.Collection;
 
-public class BishopMoveCalculator {
+public class BishopMoveCalculator extends PieceMovesCalculator{
 
     public BishopMoveCalculator() {
 
     }
 
     public static Collection<ChessMove> calculateMoves(ChessBoard board, ChessPosition position) {
-        BishopMoveCalculator calculator = new BishopMoveCalculator();
         Collection<ChessMove> possibleMoves = new ArrayList<>();
-        calculator.upAndLeft(possibleMoves, board, position);
-        calculator.upAndRight(possibleMoves, board, position);
-        calculator.downAndLeft(possibleMoves, board, position);
-        calculator.downAndRight(possibleMoves, board, position);
+        BishopMoveCalculator.upAndLeftContinuous(possibleMoves, board, position);
+        BishopMoveCalculator.upAndRightContinuous(possibleMoves, board, position);
+        BishopMoveCalculator.downAndLeftContinuous(possibleMoves, board, position);
+        BishopMoveCalculator.downAndRightContinuous(possibleMoves, board, position);
         return possibleMoves;
-    }
-
-    private void upAndLeft(Collection<ChessMove> possibleMoves, ChessBoard board, ChessPosition startPosition) {
-        PieceMovesCalculator.upAndLeftContinuous(possibleMoves, board, startPosition);
-    }
-
-    private void upAndRight(Collection<ChessMove> possibleMoves, ChessBoard board, ChessPosition startPosition) {
-        PieceMovesCalculator.upAndRightContinuous(possibleMoves, board, startPosition);
-    }
-
-    private void downAndLeft(Collection<ChessMove> possibleMoves, ChessBoard board, ChessPosition startPosition) {
-        PieceMovesCalculator.downAndLeftContinuous(possibleMoves, board, startPosition);
-    }
-
-    private void downAndRight(Collection<ChessMove> possibleMoves, ChessBoard board, ChessPosition startPosition) {
-        PieceMovesCalculator.downAndRightContinuous(possibleMoves, board, startPosition);
     }
 }
