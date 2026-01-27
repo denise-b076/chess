9a7669a45356391b5ab74d68ed787505cc276595
Denise Braithwaite <denisebraithw8@icloud.com>
2026-01-26 22:14:04 -0700

    refactor RookMoveCalculator as subclass of PieceMovesCalculator

--- shared/src/main/java/chess/calculators/RookMoveCalculator.java
+++ shared/src/main/java/chess/calculators/RookMoveCalculator.java
@@ -7,35 +7,19 @@ import chess.ChessPosition;
 import java.util.ArrayList;
 import java.util.Collection;
 
-public class RookMoveCalculator {
+public class RookMoveCalculator extends PieceMovesCalculator {
 
     public RookMoveCalculator() {
 
     }
 
     public static Collection<ChessMove> calculateMoves(ChessBoard board, ChessPosition position) {
-        RookMoveCalculator calculator = new RookMoveCalculator();
         Collection<ChessMove> possibleMoves = new ArrayList<>();
-        calculator.up(possibleMoves, board, position);
-        calculator.down(possibleMoves, board, position);
-        calculator.left(possibleMoves, board, position);
-        calculator.right(possibleMoves, board, position);
+        RookMoveCalculator.upContinuous(possibleMoves, board, position);
+        RookMoveCalculator.downContinuous(possibleMoves, board, position);
+        RookMoveCalculator.leftContinuous(possibleMoves, board, position);
+        RookMoveCalculator.rightContinuous(possibleMoves, board, position);
         return possibleMoves;
     }
 
-    private void up(Collection<ChessMove> possibleMoves, ChessBoard board, ChessPosition startPosition) {
-        PieceMovesCalculator.upContinuous(possibleMoves, board, startPosition);
-    }
-
-    private void down(Collection<ChessMove> possibleMoves, ChessBoard board, ChessPosition startPosition) {
-        PieceMovesCalculator.downContinuous(possibleMoves, board, startPosition);
-    }
-
-    private void left (Collection<ChessMove> possibleMoves, ChessBoard board, ChessPosition startPosition) {
-        PieceMovesCalculator.leftContinuous(possibleMoves, board, startPosition);
-    }
-
-    private void right (Collection<ChessMove> possibleMoves, ChessBoard board, ChessPosition startPosition) {
-        PieceMovesCalculator.rightContinuous(possibleMoves, board, startPosition);
-    }
 }
