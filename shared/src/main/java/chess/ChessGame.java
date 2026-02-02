a75f7ae13e8f905418228319974e9b74696b7ddd
Denise Braithwaite <denisebraithw8@icloud.com>
2026-02-02 14:30:19 -0700

    generated the toString(), equals(), and hashCode() methods for ChessGame

--- shared/src/main/java/chess/ChessGame.java
+++ shared/src/main/java/chess/ChessGame.java
@@ -2,6 +2,7 @@ package chess;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Objects;
 
 /**
  * For a class that can manage a chess game, making moves on a board
@@ -111,5 +112,27 @@ public class ChessGame {
      */
     public ChessBoard getBoard() {
         return currBoard;
+    }
+
+    @Override
+    public String toString() {
+        return "ChessGame{" +
+                "teamTurn=" + teamTurn +
+                ", currBoard=" + currBoard +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        ChessGame chessGame = (ChessGame) o;
+        return getTeamTurn() == chessGame.getTeamTurn() && Objects.equals(currBoard, chessGame.currBoard);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getTeamTurn(), currBoard);
     }
 }
