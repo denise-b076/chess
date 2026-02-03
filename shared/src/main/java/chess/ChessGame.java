535b3c553309f07d99976b8301e3c301e6f6c56d
Denise Braithwaite <denisebraithw8@icloud.com>
2026-02-03 11:30:21 -0700

    impl isInStalemate()

--- shared/src/main/java/chess/ChessGame.java
+++ shared/src/main/java/chess/ChessGame.java
@@ -160,7 +160,18 @@ public class ChessGame {
      * @return True if the specified team is in stalemate, otherwise false
      */
     public boolean isInStalemate(TeamColor teamColor) {
-        throw new RuntimeException("Not implemented");
+        Collection<ChessMove> totalLegalMoves = new ArrayList<>();
+        for (int i = 1; i < 9; i ++) {
+            for (int j = 1; j < 9; j++) {
+                ChessPosition currPosition = new ChessPosition(i, j);
+                ChessPiece currPiece = currBoard.getPiece(currPosition);
+                if (currPiece != null && currPiece.getTeamColor() == teamColor) {
+                    Collection<ChessMove> currLegalMoves = validMoves(currPosition);
+                    totalLegalMoves.addAll(currLegalMoves);
+                }
+            }
+        }
+        return totalLegalMoves.isEmpty() && !isInCheck(teamColor);
     }
 
     /**
