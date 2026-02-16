5509599c2f6a25dd963ea69a1a6f9b9093a08f34
Denise Braithwaite <denisebraithw8@icloud.com>
2026-02-16 15:30:50 -0700

    created request/result classes for game service

---
+++ server/src/main/java/requestresult/JoinRequest.java
@@ -0,0 +1,4 @@
+package requestresult;
+
+public record JoinRequest(String playerColor, int gameID) {
+}
---
+++ server/src/main/java/requestresult/ListResult.java
@@ -0,0 +1,8 @@
+package requestresult;
+
+import model.GameData;
+
+import java.util.ArrayList;
+
+public record ListResult(ArrayList<GameData> games) {
+}
