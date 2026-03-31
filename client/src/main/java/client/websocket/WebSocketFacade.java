5d731d3b225b9ffab006cbc1851b3cb3f24cbadf
Denise Braithwaite <denisebraithw8@icloud.com>
2026-03-30 20:01:22 -0600

    outline for NotificationHandler interface and WebSocketFacade

---
+++ client/src/main/java/client/websocket/WebSocketFacade.java
@@ -0,0 +1,38 @@
+package client.websocket;
+
+import com.google.gson.Gson;
+import jakarta.websocket.*;
+import websocket.messages.ServerMessage;
+
+import java.net.URI;
+
+public class WebSocketFacade extends Endpoint {
+
+    Session session;
+    NotificationHandler notificationHandler;
+
+    public WebSocketFacade(String url, NotificationHandler notificationHandler) throws Exception {
+        try {
+            url = url.replace("http", "ws");
+            URI socketURI = new URI(url + "/ws");
+            this.notificationHandler = notificationHandler;
+
+            WebSocketContainer container = ContainerProvider.getWebSocketContainer();
+            this.session = container.connectToServer(this, socketURI);
+
+            this.session.addMessageHandler(new MessageHandler.Whole<String>() {
+                @Override
+                public void onMessage(String message) {
+                    ServerMessage serverMessage = new Gson().fromJson(message, ServerMessage.class);
+                    notificationHandler.notify(serverMessage);
+                }
+            });
+        } catch (Exception ex) {
+            throw new Exception(ex.getMessage());
+        }
+    }
+
+    @Override
+    public void onOpen(Session session, EndpointConfig endpointConfig) {
+    }
+}
